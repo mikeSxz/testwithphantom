09b16d3de83e3f5290e20407a9f51d4392744fce
Miguel Suarez <miguel.suarez@nearshoretechnology.com>
2019-10-18 08:44:02 -0500

    updating dependencies to test

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,14 +1,12 @@
-import { TestBed, async } from '@angular/core/testing';
-import { AppComponent } from './app.component';
-describe('AppComponent', () => {
+import { TestBed, async } from "@angular/core/testing";
+import { AppComponent } from "./app.component";
+describe("AppComponent", () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [
-        AppComponent
-      ],
+      declarations: [AppComponent]
     }).compileComponents();
   }));
-  it('should create the app', async(() => {
+  it("should create the app", async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
@@ -16,12 +14,21 @@ describe('AppComponent', () => {
   it(`should have as title 'lestslearn'`, async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('lestslearn');
+    expect(app.title).toEqual("lestslearn1");
   }));
-  it('should render title in a h1 tag', async(() => {
+  it("should render title in a h1 tag", async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to lestslearn!');
+    expect(compiled.querySelector("h1").textContent).toContain(
+      "Welcome to lestslearn!"
+    );
+  }));
+  it("increase number when button pressed", async(() => {
+    const fixture = TestBed.createComponent(AppComponent);
+    fixture.detectChanges();
+    expect(fixture.componentInstance.points).toBe(1);
+    fixture.debugElement.nativeElement.querySelector("button").click();
+    expect(fixture.componentInstance.points).toBe(2);
   }));
 });
